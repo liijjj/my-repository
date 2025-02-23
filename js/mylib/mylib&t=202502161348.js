104b3a469b0d8c2f317d583c01ed2c81488189a8
liijjj <zqdlly@163.com>
2025-02-23 13:44:26 +0800

    Update mylib&t=202502161348.js

--- js/mylib/mylib&t=202502161348.js
+++ js/mylib/mylib&t=202502161348.js
@@ -5,7 +5,7 @@
 // @grant GM_getValue
 // @grant GM.xmlHttpRequest
 //==/UserScript==
-const a=3;
+const a=0;
 ;(function(win){
     const My=function(){//构造函数，需配合实例方法，与this，prototype有关
         const head=document.documentElement.firstElementChild,gsv=GM_setValue,ggv=GM_getValue,gxhr=GM.xmlHttpRequest;
